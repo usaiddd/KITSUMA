6cd1bbecde61d18fcbd13e6c14b10569270fb8a5
WarMachine108 <agarwal.tanmay2007@gmail.com>
2026-03-02 16:59:23 +0530

    updated the database and some minor testing in load.py

--- load.py
+++ load.py
@@ -5,12 +5,12 @@ try:
         host="localhost",
         database="kitsumadb",
         user="postgres",
-        password="Usaid@10",
+        password="Tanmay10",
         port="5432"
     )
 
-    login_id=input("enter your login_id :")
-    password=input("enter password :")
+    login_id=input("Enter your LoginID: ")
+    password=input("Enter your Password: ")
 
     cursor = conn.cursor()
     cursor.execute("SELECT * FROM users WHERE login = %s AND password = crypt(%s, password);",(login_id,password))
@@ -22,9 +22,22 @@ try:
     if exists:
         cursor.execute("select file_content from personalized_files where user_login=%s",(login_id,))
         output=cursor.fetchall()
+        first = True
         with open('structure.txt','w+') as f:
+            if (first):
+                f.write(login_id)
+                first = False 
             f.write(output[0][0])
-        
+        cursor.execute("SELECT file_content from file where fileno = 2")
+        output2 = cursor.fetchall()
+        if output2: 
+            with open("filecontent.txt", "w+") as f: 
+                f.write(output2[0][0])
+        with open ('define.cpp', 'r') as f:
+            filecontent = f.read()
+        cursor.execute("insert into file(filename, file_content) values (%s, %s);", ("define.cpp", filecontent))
+        cursor.execute("insert into pushes (fileno, file_content, message, user_login) values (%s, %s, %s, %s);", (1, filecontent, "TEST MESSAGE FOR NOW WILL DELETE LATER 2", login_id))
+    conn.commit() 
     cursor.close()
     conn.close()
 
